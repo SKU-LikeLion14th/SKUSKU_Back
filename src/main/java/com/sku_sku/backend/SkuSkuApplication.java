3e4f64c7551e33eb93e0fd416b7a025bbcb742dd
GOH01 <wave4446@naver.com>
2026-03-23 15:30:03 +0900

    [fix] yml mail app password

--- src/main/java/com/sku_sku/backend/SkuSkuApplication.java
+++ src/main/java/com/sku_sku/backend/SkuSkuApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class SkuSkuApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(SkuSkuApplication.class, args);
 	}
